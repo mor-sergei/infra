24b89d5c6208f926c06b8d0fd8e4fac9bbb68afd
Sergii Chebotkin <mor.sergei@gmail.com>
2019-10-22 15:41:33 +0200

    Update vmm-manage.py

--- python/vmm/vmm-manage.py
+++ python/vmm/vmm-manage.py
@@ -3,10 +3,28 @@
 import shutil
 import fileinput
 import datetime
+import argparse
+
+
+def parse_arguments():
+    """
+    Parse script input arguments.
+    :return: Object with parsed input arguments arguments.
+    """
+    arguments_parser = argparse.ArgumentParser()
+    arguments_parser.add_argument('-docpath', required=True, dest='docpath', metavar='destination_documentation_path',
+                                  type=str, help='')
+
+    arguments = arguments_parser.parse_args()
+    return arguments
 
 def replacetmpl(file, setng, savefl=False):
-    ovrFile="template/Vagrantfile"
-    shutil.copy(file, ovrFile)
+    try:
+        ovrFile='template/Vagrantfile'
+        shutil.copy(file, ovrFile)
+    except IOError:
+        print("Error: File %s does not appear to exist." % ovrFile)
+
     for line in fileinput.input(ovrFile, inplace=True):
         line = line.rstrip('\r\n')
         print(setng.get(line, line))
@@ -22,7 +40,7 @@ def main():
         '#NETPR#': 'NET_PREF = 10.11.0'
     }
 
-    templfile="template/Vagrantfile.tmpl"
+    templfile='template/Vagrantfile.tmpl'
     replacetmpl(templfile, setng)
 
 
